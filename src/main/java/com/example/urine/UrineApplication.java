dff519c3485175034f2b8fcd12825232a461cf2e
kamide28 <ghmaho0928@gmail.com>
2023-06-28 17:19:38 +0900

    first commit

---
+++ src/main/java/com/example/urine/UrineApplication.java
@@ -0,0 +1,13 @@
+package com.example.urine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urine/UrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
